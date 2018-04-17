72c721a6e286862536c0ec8fc8c4d09030b8ab9f
Fozat Kardouh <fozatkardouh@gmail.com>
2018-04-17 10:06:07 +0200

    added empty project

---
+++ src/main/java/at/refugeescode/the_magic_8_ball/TheMagic8BallApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.the_magic_8_ball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMagic8BallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMagic8BallApplication.class, args);
+	}
+}
